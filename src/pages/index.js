bead7c62a3eb816582cb7d8e6850d412cab91f34
Rohald van Merode <rohald_89@live.nl>
2022-10-16 21:14:01 +0200

    updated eslintrc

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const Section = styled.section`
 const SectionWrapper = styled.div`
     max-width: 1200px;
     margin: 0 auto;
+    padding-block: 4rem;
 `;
 
 const Wave = styled.div`
@@ -18,18 +19,24 @@ const Wave = styled.div`
     width: 100%;
     overflow: hidden;
     line-height: 0;
-    transform: rotate(180deg);
+    transform: ${({ transform }) => transform};
 
-svg {
-    position: relative;
-    display: block;
-    width: calc(143% + 1.3px);
-    height: 100px;
-}
-.shape-fill {
-    fill: ${({ theme }) => theme.background};
-}
-  `;
+    svg {
+        position: relative;
+        display: block;
+        width: ${({ width }) => width};
+        height: 100px;
+    }
+    .wave-1 {
+        fill: ${({ theme }) => theme.background};
+    }
+    .wave-2 {
+        fill: ${({ theme }) => theme.background};
+    }
+    .wave-3 {
+        fill: ${({ theme }) => theme.body};
+    }
+`;
 
 export default function Home() {
   return (
@@ -43,9 +50,9 @@ export default function Home() {
                 <h1>Welcome to FlexboxHeroes</h1>
                 <h2>Your one stop shop for all things Flexbox</h2>
             </SectionWrapper>
-            <Wave>
+            <Wave transform="rotate(180deg) matrix(-1, 0, 0, 1, 0, 0)" width="calc(143% + 1.3px)">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="wave-1"></path>
                 </svg>
             </Wave>
         </Section>
@@ -60,13 +67,18 @@ export default function Home() {
                 <h3>How do I win?</h3>
                 <p>There is no winning or losing. You can play as many times as you want. You can also use the "Solution" button to see the solution to the challenge. You can also use the "Reset" button to reset the challenge. You can use the "Next" button to move to the next challenge.</p>
             </SectionWrapper>
+            <Wave transform="rotate(180deg)" width="calc(233% + 1.3px)">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="wave-3"></path>
+                </svg>
+            </Wave>
         </Section>
         <Section color="body">
             <SectionWrapper>
                 <h3>How do I win?</h3>
                 <p>There is no winning or losing. You can play as many times as you want. You can also use the "Solution" button to see the solution to the challenge. You can also use the "Reset" button to reset the challenge. You can use the "Next" button to move to the next challenge.</p>
                 <h3>How do I win?</h3>
-                </SectionWrapper>
+            </SectionWrapper>
         </Section>
     </>
   )
