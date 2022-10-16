a173dc835b82d64288a0bac667bb30768ee75e1d
Rohald van Merode <rohald_89@live.nl>
2022-10-16 21:31:32 +0200

    temporarily removed properties due to transfering over to CMS

--- src/pages/properties/[slug].jsx
+++ src/pages/properties/[slug].jsx
@@ -1,46 +1,46 @@
-import { getAllProperties, getPropertyFromSlug } from "../../utils/mdx";
-import { serialize } from "next-mdx-remote/serialize";
-import { MDXRemote } from "next-mdx-remote"
-import { remarkCodeHike } from "@code-hike/mdx"
-import { CH } from "@code-hike/mdx/dist/components.cjs.js"
-import theme from "shiki/themes/nord.json"
-import PageWrapper from "../../styles/PageWrapper";
+// import { getAllProperties, getPropertyFromSlug } from "../../utils/mdx";
+// import { serialize } from "next-mdx-remote/serialize";
+// import { MDXRemote } from "next-mdx-remote"
+// import { remarkCodeHike } from "@code-hike/mdx"
+// import { CH } from "@code-hike/mdx/dist/components.cjs.js"
+// import theme from "shiki/themes/nord.json"
+// import PageWrapper from "../../styles/PageWrapper";
 
-export default function PropertyPage({ property }) {
-    return (
-        <PageWrapper>
-            <MDXRemote {...property.source} components={{CH}}/>
-        </PageWrapper>
-    )
-}
+// export default function PropertyPage({ property }) {
+//     return (
+//         <PageWrapper>
+//             <MDXRemote {...property.source} components={{CH}}/>
+//         </PageWrapper>
+//     )
+// }
 
-export async function getStaticProps({ params }) {
-    const { content, frontmatter } = await getPropertyFromSlug(params.slug);
+// export async function getStaticProps({ params }) {
+//     const { content, frontmatter } = await getPropertyFromSlug(params.slug);
 
-    const mdxSource = await serialize(content, {
-        mdxOptions: {
-            remarkPlugins: [[remarkCodeHike, {autoImport: false, theme }]],
-            useDynamicImport: true,
-        }
-    });
+//     const mdxSource = await serialize(content, {
+//         mdxOptions: {
+//             remarkPlugins: [[remarkCodeHike, {autoImport: false, theme }]],
+//             useDynamicImport: true,
+//         }
+//     });
 
-    return {
-        props: {
-            property: {
-                source: mdxSource,
-                frontmatter,
-            }
-        }
-    }
-}
+//     return {
+//         props: {
+//             property: {
+//                 source: mdxSource,
+//                 frontmatter,
+//             }
+//         }
+//     }
+// }
 
-export async function getStaticPaths() {
-    const properties = await getAllProperties();
+// export async function getStaticPaths() {
+//     const properties = await getAllProperties();
 
-    return {
-        paths: properties.map((property) => ({
-            params: { slug: property.slug },
-        })),
-        fallback: false,
-    }
-}
+//     return {
+//         paths: properties.map((property) => ({
+//             params: { slug: property.slug },
+//         })),
+//         fallback: false,
+//     }
+// }
--- src/pages/properties/index.jsx
+++ src/pages/properties/index.jsx
@@ -1,24 +1,24 @@
-import PageWrapper from "../../styles/PageWrapper"
+// import PageWrapper from "../../styles/PageWrapper"
 
-const { getAllProperties } = require("../../utils/mdx")
+// const { getAllProperties } = require("../../utils/mdx")
 
-export default function PropertyPage({ properties }) {
-    return (
-        <PageWrapper>
-            <h1>Properties</h1>
-            <ul>
-                {properties.map((property) => (
-                    <li key={property.slug}>
-                        <a href={`/properties/${property.slug}`}>{property.title}</a>
-                    </li>
-                ))}
-            </ul>
-        </PageWrapper>
-    )
-}
+// export default function PropertyPage({ properties }) {
+//     return (
+//         <PageWrapper>
+//             <h1>Properties</h1>
+//             <ul>
+//                 {properties.map((property) => (
+//                     <li key={property.slug}>
+//                         <a href={`/properties/${property.slug}`}>{property.title}</a>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </PageWrapper>
+//     )
+// }
 
-export async function getStaticProps() {
-    const properties = await getAllProperties()
+// export async function getStaticProps() {
+//     const properties = await getAllProperties()
 
-    return { props: { properties } }
-}
+//     return { props: { properties } }
+// }
